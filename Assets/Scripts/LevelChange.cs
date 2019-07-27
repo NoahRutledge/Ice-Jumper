9f9309cad333acf4790bc170edf0664b3984b087
NoahRutledge <noah.rutledge@gmail.com>
2019-07-27 12:15:09 -0400

    Added animation to change levels

---
+++ Assets/Scripts/LevelChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange: MonoBehaviour{
+
+    public string main_scene_name;
+
+    public void animationComplete()
+    {
+        SceneManager.LoadScene(main_scene_name);
+    }
+}
